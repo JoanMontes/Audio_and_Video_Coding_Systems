1f8a6b1a9ec617fd80ed73e3ba6c1f962d6d1017
JoanMontes <joan.montes01@estudiant.upf.edu>
2024-10-31 20:30:38 +0100

    Updated code

--- S1 - JPEG, JPEG2000 and FFMPEG/first_seminar.py
+++ S1 - JPEG, JPEG2000 and FFMPEG/first_seminar.py
@@ -1,52 +1,17 @@
 import ffmpeg
+from classes_and_methods import RGB, YCbCr
 
-class RGB:
-
-    def __init__(self, R, G, B): #Class Constructor
-        self.R = R
-        self.G = G
-        self.B = B
-    @classmethod
-    def RGB_to_YCbCr(cls):
-        #User defines R,G,B values
-        R = float(input("Choose R value: ")) 
-        G = float(input("Choose G value: "))
-        B = float(input("Choose B value: "))
-        #Apply transformation
-        Y = 0.257*R + 0.504*G + 0.098*B + 16
-        Cb = -0.148*R - 0.291*G + 0.439*B + 128
-        Cr = 0.439*R - 0.368*G - 0.071*B + 128
-        return Y, Cb, Cr
-    
-
-class YCbCr: 
-
-    def __init__(self, Y, Cb, Cr):
-        self.Y = Y
-        self.Cb = Cb
-        self.Cr = Cr
-    
-    @classmethod
-    def YCbCr_to_RGB(cls):
-        #User defines Y, Cb, Cr values
-        Y = float(input("Choose Y value: "))
-        Cb = float(input("Choose Cb value: "))
-        Cr = float(input("Choose Cr value: "))
-        #Apply transformation
-        R = 1.164*(Y - 16) + 1.596*(Cb-128)
-        G = 1.164*(Y-16) - 0.813*(Cr - 128) - 0.391*(Cb - 128)
-        B = 1.164*(Y-16) + 2.018*(Cb - 128)
-        return R, G, B
-       
+# EXERCISE 2: Creation of translator from 3 values in RGB into the 3 YUV values, plus the opposite operation
+# RGB to YCbCr
 YcrCb1 = RGB.RGB_to_YCbCr()
-print(YcrCb1)
+print("The (R, G, B) selected are converted into: ", YcrCb1, "(Y, Cb, Cr) values")
 
+# YCbCr to RGB
 RGB1 = YCbCr.YCbCr_to_RGB()
-print(RGB1)
+print("The (Y, Cb, Cr) selected are converted into: ", RGB1, "(R, G, B) values")
 
 
-
-
+# EXERCISE 3: 
 
 
 
