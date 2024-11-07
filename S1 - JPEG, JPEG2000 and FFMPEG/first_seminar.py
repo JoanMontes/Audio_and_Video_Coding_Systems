42143db28f1b5c57c6ea92f49a2f4efc12b02550
JoanMontes <joan.montes01@estudiant.upf.edu>
2024-11-07 19:47:26 +0100

    ex 5 implementation

--- S1 - JPEG, JPEG2000 and FFMPEG/first_seminar.py
+++ S1 - JPEG, JPEG2000 and FFMPEG/first_seminar.py
@@ -1,4 +1,4 @@
-from classes_and_methods import RGB, YCbCr, resize_image
+from classes_and_methods import RGB, YCbCr, resize_image, bw_image
 
 # EXERCISE 2: Creation of translator from 3 values in RGB into the 3 YUV values, plus the opposite operation
 def ex2():
@@ -15,14 +15,19 @@ def ex2():
 def ex3():
     w = int(input("Enter the width of the resized image: "))
     q = int(input("Enter the quality of the resized image: "))
-    resize_image("Input images/input.jpg", "Output images/output.jpg", w, q)
+    resize_image("Input images/input.jpg", "Output images/output_ex3.jpg", w, q)
 
-
+def ex5():
+    bw_image("Input images/input.jpg", "Output images/output_ex5.jpg")
+    
+    
 def menu():
     print("\nExercises menu:")
     print("1 - Exercise 2")
     print("2 - Exercise 3")
-    print("0 - Salir")
+    print("3 - Exercise 4")
+    print("4 - Exercise 5")
+    print("0 - Exit")
 
 while True:
     menu()
@@ -33,6 +38,9 @@ while True:
         
     elif(option == '2'):
         ex3()
+    
+    elif(option == '4'):
+        ex5()
     
     elif(option == '0'):
         print("Leaving program...")
